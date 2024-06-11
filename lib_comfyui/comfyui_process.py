07e7559209a223b200f7546dec91a176f34e1cde
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-06-11 03:08:05 +0200

    'get_base_sys_path' is not defined + TMP/TEMP

--- lib_comfyui/comfyui_process.py
+++ lib_comfyui/comfyui_process.py
@@ -62,12 +62,26 @@ def get_comfyui_executable(comfyui_install_location):
 
 def get_comfyui_env(comfyui_install_location):
     comfyui_env = os.environ.copy()
-    comfyui_sys_path = get_base_sys_path(executable, comfyui_install_location)
-    comfyui_sys_path[:0] = (str(comfyui_install_location), settings.get_extension_base_dir())
-    comfyui_env['PYTHONPATH'] = os.pathsep.join(comfyui_sys_path)
     if 'PYTHONPATH' in comfyui_env:
         del comfyui_env['PYTHONPATH']
 
+    problematic_windows_tmp_dirs = {
+        k: v
+        for k, v in comfyui_env.items()
+        if k in ("TMP", "TEMP") and v == "tmp"
+    }
+    if os.name == "nt" and problematic_windows_tmp_dirs:
+        problematic_windows_tmp_dirs = " and ".join(f'{k}="{v}"' for k, v in problematic_windows_tmp_dirs.items())
+        message_prefix = "[sd-webui-comfyui] "
+        print(
+            "\n".join([
+                f'{message_prefix}Found {problematic_windows_tmp_dirs} in the environment. On Windows, this is known to cause issues.',
+                f'{message_prefix}If ComfyUI refuses to start, consider using TEMP="temp" or TMP="temp" as a workaround.',
+                f'{message_prefix}For more information, see https://github.com/ModelSurge/sd-webui-comfyui/issues/170#issuecomment-1792009789',
+            ]),
+            file=sys.stderr,
+        )
+        
     comfyui_env['SD_WEBUI_COMFYUI_EXTENSION_DIR'] = settings.get_extension_base_dir()
     comfyui_env['SD_WEBUI_COMFYUI_IPC_STRATEGY_CLASS_NAME'] = global_state.ipc_strategy_class.__name__
     return comfyui_env
