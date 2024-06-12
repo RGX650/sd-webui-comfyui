814412a502ecaf641c2eeaa3fac9e5711258b083
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-06-12 05:58:46 +0200

    debug

--- lib_comfyui/comfyui/pre_main.py
+++ lib_comfyui/comfyui/pre_main.py
@@ -5,6 +5,8 @@ import sys
 def patch_sys_path():
     comfyui_install_dir = os.getcwd()
     extension_dir = os.getenv("SD_WEBUI_COMFYUI_EXTENSION_DIR")
+    print("[comfyui_install_dir]", f"install_dir={comfyui_install_dir}")
+    print("[extension_dir]", f"extension_dir={extension_dir}")
     if not comfyui_install_dir or not extension_dir:
         print("[sd-webui-comfyui]", f"Could not add new entries to sys.path. install_dir={comfyui_install_dir}, extension_dir={extension_dir}, sys.path={sys.path}", file=sys.stderr)
         print("[sd-webui-comfyui]", f"Exiting...", file=sys.stderr)
