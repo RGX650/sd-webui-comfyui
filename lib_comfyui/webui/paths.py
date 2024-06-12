7c65df0e2bde7849dd8a8ad18180c4dbf1dda093
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-06-12 04:59:00 +0200

    from lib_comfyui.comfyui.folder_paths

--- lib_comfyui/webui/paths.py
+++ lib_comfyui/webui/paths.py
@@ -2,28 +2,11 @@ import os
 import sys
 import importlib
 from lib_comfyui import ipc
-from pathlib import Path
 
 
 @ipc.restrict_to_process('comfyui')
 def share_webui_folder_paths():
-    from module import shared
-    # Retrieve the install location
-    install_location = shared.opts.comfyui_install_location.strip()
-    folder_paths_module_path = Path(install_location) / 'folder_paths.py'
-
-    # Ensure the folder_paths.py file exists
-    if not folder_paths_module_path.exists():
-        raise FileNotFoundError(f"{folder_paths_module_path} does not exist")
-
-    # Dynamically load the folder_paths module
-    spec = importlib.util.spec_from_file_location("folder_paths", folder_paths_module_path)
-    folder_paths = importlib.util.module_from_spec(spec)
-    sys.modules["folder_paths"] = folder_paths
-    spec.loader.exec_module(folder_paths)
-
-    # Import add_model_folder_path from the dynamically loaded module
-    from folder_paths import add_model_folder_path
+    from lib_comfyui.comfyui.folder_paths import add_model_folder_path
     webui_folder_paths = get_webui_folder_paths()
     for folder_id, folder_paths in webui_folder_paths.items():
         for folder_path in folder_paths:
